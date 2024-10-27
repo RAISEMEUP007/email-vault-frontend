e673d40316eeae44ed3b853153515667f7d88cfa
NG <anttolion@gmail.com>
2024-10-27 20:01:33 -0300

    A minor change

--- src/views/registration/VerifyCode.vue
+++ src/views/registration/VerifyCode.vue
@@ -3,7 +3,7 @@
     <div class="flex w-full max-w-1512px h-full max-h-982px">
       <div class="w-1/2 pt-24 pb-32 bg-white flex justify-center">
         <form @submit.prevent="handleSubmit" class="px-20 pt-6 pb-8 mb-4 w-full">
-          <h2 class="tracking-wide text-5xl font-black mt-20 mb-4">Multi-Factor Authentication</h2>
+          <h2 class="tracking-wide text-4xl font-black mt-20 mb-4">Multi-Factor Authentication</h2>
           <p class="mt-0.5 mb-5 text-sm pr-2">
             Please enter the confirmation code you see on your authentication app.
           </p>
